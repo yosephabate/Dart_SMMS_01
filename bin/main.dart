ed21e1d363feeca2aecceee4567f7170773154f5
Yoseph Abate <yosjudea@yahoo.com>
2018-12-13 15:20:31 +0300

    Create a new Dart Tutorial project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Dart_SMMS_01/Dart_SMMS_01.dart' as Dart_SMMS_01;
+
+main(List<String> arguments) {
+  print('Hello world: ${Dart_SMMS_01.calculate()}!');
+}
---
+++ test/Dart_SMMS_01_test.dart
@@ -0,0 +1,8 @@
+import 'package:Dart_SMMS_01/Dart_SMMS_01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

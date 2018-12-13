9e7d39ceb0e2b5177e3a3d7efefd3c51fb366f22
Yoseph Abate <yosjudea@yahoo.com>
2018-12-13 15:32:24 +0300

    Modify main.dart file

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,3 @@
-import 'package:Dart_SMMS_01/Dart_SMMS_01.dart' as Dart_SMMS_01;
-
 main(List<String> arguments) {
-  print('Hello world: ${Dart_SMMS_01.calculate()}!');
+  print('Hello world!');
 }
